efd3c15db5547f9912a96542d1d68bd9f96fe986
awoodywong <45959092+awoodywong@users.noreply.github.com>
2019-11-13 21:45:54 +0800

    add write file and complete data validating

--- OnlineBanking.cpp
+++ OnlineBanking.cpp
@@ -10,16 +10,19 @@
 
 using namespace std;
 
-void menu();
+void menu(string);
 void credits();
 void readFile(string);
-void writeFile(string);
-void openAccount();
-bool checkPhone(string);
+void writeFile_openAC(string);
+bool checkPhone(int);
 bool checkName(string);
 bool checkPassword(string);
+string acName_input();
+int phNum_input();
+string acPw_input();
 int genAccNum(int);
-int hashPW(string);
+double balance_input();
+void inputReq();
 
 /*--------------------------------------class--------------------------------------*/
 
@@ -45,11 +48,9 @@ int main() {
 	cout << "Welcome Message designed by Group ___" << endl;
 	cout << "Please input the file name of the data file: ";
 	cin >> file_name;
-
 	//readFile(file_name);
-	//writeFile(file_name);
-
-	menu();
+	//system("pause");
+	menu(file_name);
 
 	return 0;
 }
@@ -58,7 +59,8 @@ int main() {
 
 //credits
 void credits() {
-	cout << left << setw(20) << "name" << setw(15) << "id" << setw(10) << "class" << setw(20) << "tutorial group" << endl;
+	system("cls");
+	cout << left << setw(20) << "Name" << setw(15) << "ID" << setw(10) << "Class" << setw(20) << "Tutorial Group" << endl;
 	cout << "-----------------------------------------------------------" << endl;
 	cout << left << setw(20) << "Wong Kong Lun" << setw(15) << "18039547A" << setw(10) << "class" << setw(20) << "tut class" << endl;
 	cout << left << setw(20) << "name" << setw(15) << "student ID" << setw(10) << "class" << setw(20) << "tut class" << endl;
@@ -68,9 +70,9 @@ void credits() {
 }
 
 //menu
-void menu() {
+void menu(string file_name) {
 	string menuOption;
-
+	system("cls");
 	cout << "*** Main Menu ***" << endl;
 	cout << "[1] Open Account" << endl;
 	cout << "[2] Login" << endl;
@@ -79,16 +81,19 @@ void menu() {
 	cout << "[5] Exist" << endl;
 	cout << "*****************" << endl;
 	cout << "Option (1 - 5): ";
-	cin >> menuOption;
+	cin >> menuOption; 
 
 	if (menuOption == "1") {
-		openAccount();
+		system("cls");
+		inputReq();
+		writeFile_openAC(file_name);
 	}
 	else if (menuOption == "2") {
 
 	}
 	else if (menuOption == "3") {
-
+		system("cls");
+		readFile(file_name);
 	}
 	else if (menuOption == "4") {
 		credits();
@@ -121,22 +126,26 @@ void readFile(string file_name) {
 	inFile.close();
 }
 
-void writeFile(string file_name) {
+void writeFile_openAC(string file_name) {
 	// set up the hash function
 	hash<string> str_hash;
-
 	// open the file
+	int acc_no, tel;
+	string name, pw;
+	double balance;
+	size_t hash_val;
 	ofstream outFile;
 
 	// file is written and put in same folder as source code
 	outFile.open(file_name);
 
 	// data fields to write into file
-	int acc_no = 567987654;
-	string name = "Wong Siu Ming";
-	int tel = 60123456;
-	size_t hash_val = str_hash("123456");    // hash value has type "size_t"
-	double balance = 1300000.12;
+	name = acName_input();
+	tel = phNum_input();
+	pw = acPw_input();
+	acc_no = genAccNum(tel);
+	hash_val = str_hash(pw);
+	balance = balance_input();
 
 	// ensure floating point is written in 2 d.p.
 	outFile.setf(ios::fixed, ios::floatfield);
@@ -196,18 +205,12 @@ void bankMenu(string accountName, int accountNumber) {
 }
 
 
-void openAccount() {
+string acName_input() {
 	string accountName, accountName_confirm;
-	string phoneNumber, phoneNumber_confirm;
-	string accountPassword, accountPassword_confirm;
-	double accountBalance;
-	system("cls");
-	/*-----------------------------account holde name------------------------------------------*/
-
-		 //do-while loop  for checking the name requirment
+	//do-while loop  for checking the name requirment
 	do {
-        cin.ignore(256, '\n');
-        cin.clear();
+		cin.clear();
+		cin.ignore(256, '\n');
 		//do-while loop for input account holder name and confirm
 		do {
 			if (accountName_confirm != accountName) {
@@ -222,41 +225,74 @@ void openAccount() {
 			getline(cin, accountName_confirm);
 		} while (accountName_confirm != accountName);
 
-        //call the function checkName for check the name requirment
+		//call the function checkName for check the name requirment
 	} while (!checkName(accountName));
 
 	cout << endl;
+	return accountName;
+}
 	/*------------------------account holder phone number--------------------------------------*/
+int phNum_input() {
+	int phoneNumber = 0, phoneNumber_confirm = phoneNumber;
 	do {
-        cin.ignore(256, '\n');
-        cin.clear();
+		cin.clear();
+		cin.ignore(256, '\n');
 		do {
+
 			if (phoneNumber_confirm != phoneNumber) {
 				cout << endl;
-				cout << "Not Match, Please Enter Again" << endl;
+				cout << "Not Match, Please Enter Again." << endl;
 				cout << endl;
 			}
-			cout << "Enter Your Mobile Phone Number: ";
-			getline(cin, phoneNumber);
 
-			cout << "Confirm Your Mobile Phone Number: ";
-			getline(cin, phoneNumber_confirm);
-		} while (phoneNumber_confirm != phoneNumber);
-	} while (!checkPhone(phoneNumber));
-	/*-------------------------account password------------------------------------------------*/
+			cout << "Enter Your Phone Number: ";
+			while (!(cin >> phoneNumber) || phoneNumber <= 0) {
+				cout << endl;
+				cout << "Not Match The Phone Number Requirement." << endl;
+				cout << "Please Enter Again." << endl;
+				cout << endl;
+				cout << "Enter Your Phone Number: ";
+				cin.clear(); //Reset the input error status to no error
+				cin.ignore(256, '\n'); //Discard the content in the input sequence
+			}
+
+			cout << "Confirm Your Phone Number: ";
+			while (!(cin >> phoneNumber_confirm) || phoneNumber <= 0) {
+				cout << endl;
+				cout << "Not Match The Phone Number Requirement." << endl;
+				cout << "Please Enter Again." << endl;
+				cout << endl;
+				cout << "Confirm Your Phone Number: ";
+				cin.clear(); //Reset the input error status to no error
+				cin.ignore(256, '\n');//Discard the content in the input sequence
+			}
+		}
+		//while loop if phone number and the confirm is not the same
+		while (phoneNumber_confirm != phoneNumber);
+
+	}
+	//do-while loop for checking the digit of the phone number
+	while (!checkPhone(phoneNumber));
+
 	cout << endl;
 
+	return phoneNumber;
+}
+
+/*-------------------------account password------------------------------------------------*/
+string acPw_input() {
+
+	string accountPassword, accountPassword_confirm;
 	//do-while loop for checking the password requirement
 	do {
-        cin.ignore(256, '\n');
-        cin.clear();
+		cin.clear();
+		cin.ignore(256, '\n');
 		//do-while loop for  confirm the password
 		do {
 			if (accountPassword_confirm != accountPassword) {
 				cout << endl;
 				cout << "Not Match, Please Enter Again" << endl;
 				cout << endl;
-
 			}
 			cout << "Enter Your Account Password: ";
 			getline(cin, accountPassword);
@@ -265,41 +301,47 @@ void openAccount() {
 			getline(cin, accountPassword_confirm);
 		} while (accountPassword_confirm != accountPassword);
 	} while (!checkPassword(accountPassword));
-	system("cls");
+	
+	cout << endl;
+
+	return accountPassword;
+}
+
+double balance_input() {
+	double bal;
+
+	do {
+		cout << "Initial Amount to Deposit (At Least $100) : $";
+		cin >> bal;
+		if (bal < 100) {
+			cout << endl;
+			cout << "The Initial Amount Must Be At Least $100" << endl;
+			cout << "Please Enter Again" << endl;
+			cout << endl;
+		}
+	} 
+	while (bal < 100);
+	return bal;
 }
 
 //the function of checking the phone number requirement
-bool checkPhone(string s){
-
-	//let array cstr to contain the phone number
-	char *cstr = new char[s.length() + 1];
-
-	//copy string s to cstr array
-	strcpy(cstr, s.c_str());
-
+bool checkPhone(int num) {
 	int counter = 0;
 
-	//for-loop for checking the phone requirement
-	for (int i = 0; i < s.length(); i++) {
-		if ((int(cstr[i]) >= 48) && (int(cstr[i]) <= 57) && s.length() == 8) {
-		}
-		else {
-			counter++;
-		}
+	while (num > 0) {
+		num /= 10;
+		counter++;
 	}
 
-	//the condition of checking
-	if (counter > 0) {
+	if (counter == 8) {
+		return 1;
+	}
+	else {
 		cout << endl;
 		cout << "Not Match The Phone Number Requirement." << endl;
 		cout << "Please Enter Again." << endl;
-        cout << "Press Enter Button To Continue";
-        system("pause");
 		cout << endl;
 		return 0;
-	}
-	else {
-		return 1;
 	}
 }
 
@@ -316,7 +358,7 @@ bool checkName(string s) {
 
 
 	for (int i = 0; i < s.length(); i++) {
-		if (int(cstr[i]) == 32 || (int(cstr[i]) >= 65 && int(cstr[i]) <= 90) || (int(cstr[i]) >= 97 && int(cstr[i]) <= 122)) {
+		if (int(cstr[i]) == 32 || (int(cstr[i]) >= 65 && int(cstr[i]) <= 90) || (int(cstr[i]) >= 97 && int(cstr[i]) <= 122) || s.length() > 20) {
 		}
 		else {
 			counter++;
@@ -327,8 +369,6 @@ bool checkName(string s) {
 		cout << endl;
 		cout << "Not Match The Name Requirement." << endl;
 		cout << "Please Enter Again." << endl;
-        cout << "Press Enter Button To Continue";
-        system("pause");
 		cout << endl;
 		return 0;
 	}
@@ -361,8 +401,6 @@ bool checkPassword(string s) {
 		cout << endl;
 		cout << "Not Match The Password Requirement." << endl;
 		cout << "Please Enter Again." << endl;
-        cout << "Press Enter Button To Continue";
-        system("pause");
 		cout << endl;
 		return 0;
 	}
@@ -382,10 +420,19 @@ int genAccNum(int phNum) {
 	return acNum;
 }
 
-int hashPW(string PW) {
-	int hashPw;
-	hash<string> str_hash;
-	hashPw = str_hash(PW);
-	return hashPw;
+void inputReq() {
+	cout << "***************Information Requirement***************" << endl;
+	cout << setw(30) << left << "Account Holder Name:" << "The Account Holder Name Does Not Containe Any Digits" << endl;
+	cout << setw(30) << " " << " Or Other Symbols Except The Alphabets And The Space Charactor. It Should Be At" << endl;
+	cout << setw(30) << " " << " Most 20-Charater Long." << endl;
+	cout << endl;
+	cout << setw(30) << left << "Mobile Phone Number:" << "Mobile Phone Number Shoould Be An 8-Digit Input." << endl;
+	cout << endl;
+	cout << setw(30) << left << "Account Password: " << "Account Password Should Be Of At Least 8-Charater Long, Which" << endl;
+	cout << setw(30) << " " << " Includes At Least One Capital Letter, One Small Letter And One Digit." << endl;
+	cout << endl;
+	cout << setw(30) << left << "Initial Amount:" << "The Initial Amount To Deposit Should Be At Least $100." << endl;
+	cout << "*****************************************************" << endl;
 }
+
 
